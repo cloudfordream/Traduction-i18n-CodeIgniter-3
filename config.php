bb16c7ea2bb8287263d7f40b44deafb5b2d00993
Cloudfordream <cloudfordream@gmail.com>
2020-08-04 21:22:51 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+>,php
+
+$config['language']	= 'english';
+
+/* default language abbreviation */
+$config['language_abbr'] = "en";
+
+/* set available language abbreviations */
+$config['lang_available'] = [
+    'fr' => 'french',
+    'en' => 'english',
+    'ru' => 'russian'
+];
+
+/* ignore uri list */
+$config['lang_uri_ignore'] = [
+    ''
+];
+
+/* time to store cookie lang */
+$config['lang_time'] = 2592000;
