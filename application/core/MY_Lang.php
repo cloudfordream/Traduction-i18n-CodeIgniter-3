43de92a366244c6aa693e41be3dca806328cbbe4
Cloudfordream <cloudfordream@gmail.com>
2020-08-05 03:26:27 +0200

    Update MY_Lang.php

--- application/core/MY_Lang.php
+++ application/core/MY_Lang.php
@@ -5,7 +5,7 @@ class MY_Lang extends CI_Lang
 
     private $config;
     
-    private $index_page;
+    private $lang_abbr;
     private $default_abbr;
     private $lang_available;
     private $lang_uri_ignore;
@@ -21,94 +21,76 @@ class MY_Lang extends CI_Lang
         
         $this->config =& $CFG->config;
         
-        $this->index_page      = $this->config['index_page'];
         $this->default_abbr    = $this->config['language_abbr'];
         $this->lang_available  = $this->config['lang_available'];
         $this->lang_uri_ignore = $this->config['lang_uri_ignore'];
+        $this->uri_abbr        = (strlen($URI->segment(1)) === 2) ? $URI->segment(1) : NULL;
 
-        $this->uri_abbr = $URI->segment(1);
-
-        /* contril if lang is set in url and end slash is set */
-        if(strlen($this->uri_abbr) === 2 AND empty($URI->segment(2)) AND substr($IN->server('REQUEST_URI'), -1) !== '/'){
-
-            header('Location: '.$this->config['base_url'].$this->index_page.$URI->uri_string.'/');
-
-        }
-
-        /* verify if uri is in lang_uri_ignore */
-        if(!in_array($this->uri_abbr, $this->lang_uri_ignore, TRUE)){
-
-            /* adjust the uri string leading slash */
-            $URI->uri_string = preg_replace("|^\/?|", '/', $URI->uri_string);
-
-            /* control if lang cookie exist */
-            if(!is_null($IN->cookie('user_lang')) AND !isset($this->uri_abbr)){
-
-                /* set lang in uri */    
-                $this->index_page .= empty($this->index_page) ? $IN->cookie('user_lang', TRUE) : "/".$IN->cookie('user_lang', TRUE);
-                
-                if(strlen($this->uri_abbr) === 2){
-
-                    /* remove invalid abbreviation */
-                    $URI->uri_string = preg_replace("|^\/?$this->uri_abbr|", '', $URI->uri_string);
-
-                }
-                
-                /* redirect */
-                header('Location: '.$this->config['base_url'].$this->index_page.$URI->uri_string.'');
-
-                exit();
-
-                /* set the language_abbreviation cookie */                
-                $IN->set_cookie('user_lang', $this->default_abbr, $this->config['lang_time']);
-
+        /* determine lang */
+        if(empty($IN->cookie('user_lang', TRUE))){
+            if(isset($this->lang_available[substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2)])){
+                $this->default_lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
             }else{
-
-                /* check validity against config array */
-                if(isset($this->lang_available[$this->uri_abbr])){
-            
-                    /* reset uri segments and uri string */
-                    $URI->segment(array_shift($URI->segments));
-                    $URI->uri_string = preg_replace("|^\/?$this->uri_abbr|", '', $URI->uri_string);
-                    
-                    /* set config language values to match the user language */
-                    $this->config['language'] = $this->lang_available[$this->uri_abbr];
-                    $this->config['language_abbr'] = $this->uri_abbr;
-                            
-                    /* check and set the uri identifier */
-                    $this->index_page .= empty($this->index_page) ? $this->uri_abbr : "/$this->uri_abbr";
-        
-                    /* set the language abbreviation cookie */               
-                    $IN->set_cookie('user_lang', $this->uri_abbr, $this->config['lang_time']);
-                
-                } else { 
-
-                    /* check if cookie exist */
-                    (!empty($IN->cookie('user_lang', TRUE))) ? $bypass_abbr = $IN->cookie('user_lang', TRUE) : $bypass_abbr = $this->default_abbr;
-
-                    /* check and set the uri identifier to the default value */
-                    $this->index_page .= empty($this->index_page) ? $bypass_abbr : "/$bypass_abbr";
-                    
-                    if(strlen($this->uri_abbr) === 2){
-
-                        /* remove invalid abbreviation */
-                        $URI->uri_string = preg_replace("|^\/?$this->uri_abbr|", '', $URI->uri_string).'';
-
-                    }
-                    
-                    /* redirect */
-                    (!empty($URI->uri_string)) ? header('Location: '.$this->config['base_url'].$this->index_page.$URI->uri_string) : header('Location: '.$this->config['base_url'].$this->index_page.'/'.$URI->uri_string);
-                    
-                    exit();
-    
-                    /* set the language abbreviation cookie */                
-                    $IN->set_cookie('user_lang', $this->default_abbr, $this->config['lang_time']);
-                    
-                }
-                
-                log_message('debug', "Language_Identifier Class Initialized");
+                $this->default_lang = $this->default_abbr;
+            }
+        }else{
+            if(!array_key_exists($this->uri_abbr, $this->lang_available)){
+                $this->default_lang = $this->default_abbr;
+            }else{
+                $this->default_lang = $IN->cookie('user_lang', TRUE);
             }
         }
+
+        /* control if uri isn't in lang_uri_ignore */
+        if(!in_array($this->default_lang, $this->lang_uri_ignore, TRUE)){
+            /* control if lang is not set in url */
+            if(empty($this->uri_abbr)){
+                /* set lang in uri */    
+                $this->lang_abbr .= empty($this->lang_abbr) ? $this->default_lang : "/".$this->default_lang;
+
+                $this->set_lang($this->default_lang);
+
+                /* redirect */
+                header('Location: '.$this->config['base_url'].$this->lang_abbr.'/'.$URI->uri_string);
+
+                exit();
+            }else{
+                /* if lang is not available */
+                if(!array_key_exists($this->uri_abbr, $this->lang_available)){
+                    $URI->uri_string = preg_replace('/^\/?' . $this->uri_abbr . '/', $this->default_lang, $URI->uri_string);
+
+                    $this->set_lang($this->default_lang);
+
+                    /* redirect */
+                    header('Location: '.$this->config['base_url'].$this->lang_abbr.$URI->uri_string);
+
+                    exit();
+                }else{
+                    $this->set_lang($this->uri_abbr);
+                }
+            }
+        }
+
+        log_message('debug', "Language_Identifier Class Initialized");
+
+        /* control if lang is set in url and end slash is set */
+        if(strlen($this->uri_abbr) === 2 AND empty($URI->segment(2)) AND substr($IN->server('REQUEST_URI'), -1) !== '/'){
+            /* redirect */
+            header('Location: '.$this->config['base_url'].$this->lang_abbr.$URI->uri_string.'/');
+        }
+    }
+
+    /* set lang */
+    private function set_lang($abbr)
+    {
+        global $IN;
+
+        /* set cookie with lang */
+        $IN->set_cookie('user_lang', $abbr, $this->config['lang_time']);
+
+        /* set config language values to match the user language */
+        $this->config['language'] = $this->lang_available[$abbr];
+        $this->config['language_abbr'] = $abbr;
     }
 
     /* return currently lang set */
@@ -120,11 +102,11 @@ class MY_Lang extends CI_Lang
     /* return url for anchor and switch lang */
     public function switch_lang($lang)
     {
-        global $URI;
+        global $URI, $IN;
 
-        (substr($URI->uri_string, 0, 1) !== '/') ? $slash = '/' : $slash = NULL;
+        $URI->uri_string = preg_replace('/^\/?' . $this->uri_abbr . '/', $lang, $URI->uri_string);
 
-        return $lang.$slash.$URI->uri_string;
+        return $URI->uri_string;
     }
 
     /* translate helper */
