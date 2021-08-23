2683b9e25f01710dafdff971d15b6e81ad517a73
Cloudfordream <cloudfordream@gmail.com>
2021-08-23 09:48:30 +0200

    Update MY_Lang.php

--- application/core/MY_Lang.php
+++ application/core/MY_Lang.php
@@ -1,14 +1,27 @@
-<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+<?php
 
-class MY_Lang extends CI_Lang
-{
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Class My_Lang
+ * 
+ * @author Maxime Delalande
+ * @link https://github.com/cloudfordream
+ */
+
+class MY_Lang extends CI_Lang {
 
     private $config;
-    
-    private $lang_abbr;
-    private $default_abbr;
-    private $lang_available;
-    private $lang_uri_ignore;
+
+    private $language_abbr;
+
+    private $default_language;
+
+    private $default_language_abbr;
+
+    private $language_available;
+
+    private $language_uri_ignore;
 
     private $uri_abbr;
 
@@ -16,130 +29,286 @@ class MY_Lang extends CI_Lang
 
     public function __construct()
     {
-
         parent::__construct();
-        
+
         global $URI, $CFG, $IN;
-        
+
         $this->config =& $CFG->config;
-        
-        $this->default_abbr    = $this->config['language_abbr'];
-        $this->lang_available  = $this->config['lang_available'];
-        $this->lang_uri_ignore = $this->config['lang_uri_ignore'];
-        $this->uri_abbr        = (strlen($URI->segment(1)) === 2) ? $URI->segment(1) : NULL;
-        $this->query_string    = (!empty($IN->server('QUERY_STRING'))) ? '?' . $IN->server('QUERY_STRING') : NULL;
 
-        /* control if uri isn't in lang_uri_ignore */
-        if(!in_array($URI->segment(1), $this->lang_uri_ignore, TRUE)){
+        $this->language_abbr = $this->config['language_abbr'];
 
-            /* determine lang */
-            if(empty($IN->cookie('user_lang', TRUE))){
-                if(isset($this->lang_available[substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2)])){
-                    $this->default_lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+        $this->default_language = $this->config['language'];
+
+        $this->default_language_abbr = $this->config['language_abbr'];
+
+        $this->language_available = $this->config['language_available'];
+
+        $this->language_uri_ignore = $this->config['language_uri_ignore'];
+
+        $this->uri_abbr = (strlen($URI->segment(1)) === 2) ? $URI->segment(1) : NULL;
+
+        $this->query_string = (!empty($IN->server('QUERY_STRING'))) ? '?' . $IN->server('QUERY_STRING') : NULL;
+
+        /**
+         * Control if uri must be ignored
+         */
+        foreach($this->language_uri_ignore as $uri){
+            if(preg_match('/' . preg_replace('/\//', '\/', $uri) . '/i', $URI->uri_string()) === 0 OR is_null($uri)){
+                /**
+                 * The uri should not be ignored
+                 */
+                $ignore = FALSE;
+            }else{
+                /**
+                 * Uri should be ignored
+                 */
+                $ignore = TRUE;
+
+                break;
+            }
+        }
+
+        if(!$ignore){
+            /**
+             * Control cookie if set
+             */
+            if(!empty($IN->cookie('user_language', TRUE))){
+                /**
+                 * Cookie exist
+                 */
+                if(!array_key_exists($IN->cookie('user_language', TRUE), $this->language_available)){
+                    /**
+                     * If lang in cookie isn't available set default abbr
+                     */
+                    $this->language_abbr = $this->default_language_abbr;
                 }else{
-                    $this->default_lang = $this->default_abbr;
+                    /**
+                     * If lang in cookie is avaible set abbr
+                     */
+                    $this->language_abbr = $IN->cookie('user_language', TRUE);
                 }
             }else{
-                if(!isset($this->lang_available[$IN->cookie('user_lang', TRUE)])){
-                    $this->default_lang = $this->default_abbr;
+                /**
+                 * Cookie dosen't exist
+                 */
+                if(!empty($_SERVER['HTTP_ACCEPT_LANGUAGE']) AND array_key_exists(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2), $this->language_available)){
+                    /**
+                     * If user browser language is available set abbr
+                     */
+                    $this->language_abbr = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
                 }else{
-                    $this->default_lang = $IN->cookie('user_lang', TRUE);
+                    /**
+                     * If user browser language isn't available set default abbr
+                     */
+                    $this->language_abbr = $this->default_language_abbr;
                 }
             }
 
-            /* control if lang is not set in url */
-            if(empty($this->uri_abbr)){
-                /* set lang in uri */    
-                $this->lang_abbr .= empty($this->lang_abbr) ? $this->default_lang : "/".$this->default_lang;
+            /**
+             * Control if language is set in url
+             */
+            if(!empty($this->uri_abbr)){
+                /**
+                 * Language is set, control if available
+                 */
+                if(!array_key_exists($this->uri_abbr, $this->language_available)){
+                    /**
+                     * Language isn't available, redirect user to default language
+                     */
+                    $URI->uri_string = preg_replace('/^\/?' . $this->uri_abbr . '/', $this->default_language_abbr, $URI->uri_string);
 
-                $this->set_lang($this->default_lang);
+                    $this->set_language($this->default_language_abbr);
 
-                /* redirect */
-                header('Location: '.$this->config['base_url'].$this->lang_abbr.'/'.$URI->uri_string.$this->query_string);
-
-                exit();
-            }else{
-                /* if lang is not available */
-                if(!isset($this->lang_available[$this->uri_abbr])){
-                    $URI->uri_string = preg_replace('/^\/?' . $this->uri_abbr . '/', $this->default_lang, $URI->uri_string);
-
-                    $this->set_lang($this->default_lang);
-
-                    /* redirect */
                     header('Location: '.$this->config['base_url'].$URI->uri_string.$this->query_string);
 
                     exit();
                 }else{
-                    $this->set_lang($this->uri_abbr);
+                    /**
+                     * Language is available set cookie
+                     */
+                    $this->set_language($this->uri_abbr);
                 }
-            }
-        }
+            }else{
+                /**
+                 * Set language in cookie
+                 */
+                $this->set_language($this->language_abbr);
 
-        log_message('debug', "Language_Identifier Class Initialized");
-        
-        /* control if lang is set in url and end slash is set */
-        if(strlen($this->uri_abbr) === 2 AND empty($URI->segment(2)) AND substr($IN->server('REQUEST_URI', TRUE), -1) !== '/'){
-            /* redirect */
-            header('Location: '.$this->config['base_url'].$this->lang_abbr.$URI->uri_string.'/'.$this->query_string);
+                header('Location: '.$this->config['base_url'].$this->language_abbr.'/'.$URI->uri_string.$this->query_string);
+
+                exit();
+            }
+
+            log_message('debug', "Language i18n Class Initialized");
+
+            /**
+             * Control if language is set in url and is end by slash
+             */
+            if(strlen($this->uri_abbr) === 2 AND empty($URI->segment(2)) AND substr($IN->server('REQUEST_URI'), -1) !== '/'){
+                header('Location: '.$this->config['base_url'].$this->uri_abbr.'/'.$this->query_string);
+            }
         }
     }
 
-    /* set lang */
-    private function set_lang($abbr)
+    /**
+     * Create cookie for language
+     * 
+     * @param string $language_abbr
+     */
+    private function set_language(string $language_abbr)
     {
         global $IN;
 
-        /* set cookie with lang */
-        $IN->set_cookie('user_lang', $abbr, $this->config['lang_time']);
+        $IN->set_cookie('user_language', $language_abbr, $this->config['language_save_time']);
 
-        /* set config language values to match the user language */
-        $this->config['language'] = $this->lang_available[$abbr];
-        $this->config['language_abbr'] = $abbr;
+        $this->config['language'] = $this->language_available[$language_abbr];
+        $this->config['language_abbr'] = $language_abbr;
     }
 
-    /* return currently lang set */
-    public function lang()
+    /**
+     * Retrieve current user language
+     * 
+     * @return string
+     */
+
+    public function language(): string
     {
         return $this->config['language_abbr'];
     }
 
-    /* return url for anchor and switch lang */
-    public function switch_lang($lang, $uri = NULL)
+    /**
+     * Language switcher
+     * 
+     * @param string $language_abbr
+     * @param string $uri
+     * 
+     * @return string
+     */
+
+    public function switch_language(string $language_abbr, string $uri = NULL): string
     {
         global $URI;
 
-        if($uri){
-            $URI->uri_string = base_url() . $lang . '/' . $uri;
+        if(!is_null($uri)){
+            $URI->uri_string = base_url() . $language_abbr . '/' . $uri;
         }else{
-            $URI->uri_string = preg_replace('/^\/?' . $this->uri_abbr . '/', $lang, $URI->uri_string);
+            $URI->uri_string = preg_replace('/^\/?' . $this->uri_abbr . '/', $language_abbr, $URI->uri_string . $this->query_string);
         }
 
         return $URI->uri_string;
     }
-
-    /* translate helper */
-    public function t($line)
-    {
-        global $LANG;
-        return ($t = $LANG->line($line)) ? $t : $line;
-    }
     
-    public function u($line)
+    /**
+     * Helper for retrieve text translation
+     * 
+     * @param string $line
+     * @param array $dynamics Use this format : ['dynamics_key' => 'value']
+     * 
+     * @return string
+     */
+
+    public function translation(string $line, array $dynamics = []): string
     {
         global $LANG;
-        return ($u = $LANG->line($line)) ? base_url() . $this->lang() . '/' . $u : $line;
-    }
 
-    public function u_list($name = NULL)
-    {
-        global $CFG;
+        if(isset($dynamics)){
+            $array = $dynamics;
 
-        foreach($CFG->config['lang_available'] as $lang_abbr => $lang_name){
-            require APPPATH . 'language/' . $lang_name . '/core/url_lang.php';
+            $return = preg_replace_callback('/{(.*?)}/', function($matches) use ($array){
+                $match = $matches[0];
+                $name = $matches[1];
 
-            $return[$lang_abbr] = (!empty($lang[$name])) ? $lang[$name] : NULL;
+                return isset($array[$name]) ? $array[$name] : $match;
+            }, $LANG->line($line));
+        }else{
+            $return = ($translation = $LANG->line($line)) ? $translation : $line;
         }
 
         return $return;
     }
+
+    /**
+     * Helper for retrieve url in current language
+     * 
+     * @param string $line
+     * @param array $dynamics 
+     * 
+     * Use this format : 
+     *  [
+     *      'fr' => [
+     *          'dynamics_key' => 'value'    
+     *      ],
+     *      'en' => [
+     *          'dynamics_key' => 'value'    
+     *      ]
+     *  ]
+     * 
+     * @return string
+     */
+
+    public function url(string $line, array $dynamics = []): string
+    {
+        global $LANG;
+
+        if(isset($dynamics[$this->language()])){
+            $array = $dynamics[$this->language()];
+
+            $return = preg_replace_callback('/{(.*?)}/', function($matches) use ($array){
+                $match = $matches[0];
+                $name = $matches[1];
+
+                return isset($array[$name]) ? $array[$name] : $match;
+            }, base_url() . $this->language() . '/' . $LANG->line($line));               
+        }else{
+            $return = ($url = $LANG->line($line)) ? base_url() . $this->language() . '/' . $url : $line;
+        }
+
+        return $return;
+    }
+
+    /**
+     * Helper for retrieve url in all language
+     * 
+     * @param string $line
+     * @param array $dynamics
+     * 
+     * Use this format : 
+     *  [
+     *      'fr' => [
+     *          'dynamics_key' => 'value'    
+     *      ],
+     *      'en' => [
+     *          'dynamics_key' => 'value'    
+     *      ]
+     *  ]
+     * 
+     * @return array
+     */
+
+    public function url_list(string $line, array $dynamics = []): array
+    {
+        global $CFG;
+
+        foreach($CFG->config['language_available'] as $language_abbr => $language_name){
+            require APPPATH . 'language/' . $language_name . '/core/url_lang.php';
+
+            if(isset($dynamics[$language_abbr])){
+                $array = $dynamics[$language_abbr];
+
+                $return[$language_abbr] = preg_replace_callback('/{(.*?)}/', function($matches) use ($array){
+                    $match = $matches[0];
+                    $name = $matches[1];
+
+                    return isset($array[$name]) ? $array[$name] : $match;
+                }, base_url() . $language_abbr . '/' . $lang[$line]);
+            }else{
+                $return[$language_abbr] = (!empty($lang[$name])) ? $lang[$name] : NULL;
+            }
+        }
+
+        return $return;
+    }
+
 }
+
+?>
